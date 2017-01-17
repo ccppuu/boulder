113255adfddb162a81916d90d9d332755fe69d26
Daniel <cpu@letsencrypt.org>
2017-01-17 12:43:07 -0500

    Comments

--- test/gsb-test-srv/main.go
+++ test/gsb-test-srv/main.go
@@ -17,9 +17,11 @@ import (
 	gsb_proto "github.com/letsencrypt/boulder/test/gsb-test-srv/proto"
 )
 
+// testSrv implements a bare bones mock Google Safe Browsing server. The `hits`
+// and `hp` fields are protected against concurrent updates using `mu`.
 type testSrv struct {
 	apiKey string
-	hp     hashPrefixes
+	list   safebrowsingList
 	hits   map[string]int
 	mu     *sync.RWMutex
 }
@@ -28,14 +30,21 @@ const (
 	protoMime = "application/x-protobuf"
 )
 
-// An empty threat list update response for padding responses out
+// defaultUnsafeURLs is the list of URLs that we return a "unsafe" response for.
+// Additional URLs are added by sending POSTs to the `/add` HTTP endpoint.
+var defaultUnsafeURLs = []string{
+	"honest.achmeds.discount.hosting.com",
+}
+
+// emptyThreatListUpdateResp is an empty threat list update response for padding
+// responses out to include the correct number of list updates clients expect
 var emptyThreatListUpdateResp = &gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse{
 	ThreatType:      gsb_proto.ThreatType_MALWARE,
 	PlatformType:    gsb_proto.PlatformType_ANY_PLATFORM,
 	ThreatEntryType: gsb_proto.ThreatEntryType_URL,
 	ResponseType:    gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse_FULL_UPDATE,
 	/*
-	 * This is the SHA1Sum of `[]byte{}`, e.g. an empty update
+	 * This is the SHA1Sum of `[]byte{}`, e.g. of an empty list of additions
 	 */
 	Checksum: &gsb_proto.Checksum{
 		Sha256: []byte{
@@ -46,7 +55,8 @@ var emptyThreatListUpdateResp = &gsb_proto.FetchThreatListUpdatesResponse_ListUp
 	},
 }
 
-func unmarshal(req *http.Request, pbReq proto.Message) error {
+// unmarshalPB unmarshals a request body into a protocol buffer message
+func unmarshalPB(req *http.Request, pbReq proto.Message) error {
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		return err
@@ -57,7 +67,8 @@ func unmarshal(req *http.Request, pbReq proto.Message) error {
 	return nil
 }
 
-func marshal(resp http.ResponseWriter, pbResp proto.Message) error {
+// marshalPB marshals a protocol buffer message into a response body
+func marshalPB(resp http.ResponseWriter, pbResp proto.Message) error {
 	resp.Header().Set("Content-Type", protoMime)
 	body, err := proto.Marshal(pbResp)
 	if err != nil {
@@ -69,137 +80,167 @@ func marshal(resp http.ResponseWriter, pbResp proto.Message) error {
 	return nil
 }
 
-type hashPrefix struct {
+// listEntry represents an entry on the safe browsing list. Each entry is a hash
+// constructed out of the URL.
+type listEntry struct {
 	hash string
 	url  string
 }
 
-func newHashPrefix(pattern string) hashPrefix {
+// newListEntry creates a listEntry out of a URL
+func newListEntry(url string) listEntry {
 	hash := sha256.New()
-	hash.Write([]byte(pattern))
-	return hashPrefix{
+	hash.Write([]byte(url))
+	return listEntry{
 		hash: string(hash.Sum(nil)),
-		url:  pattern,
+		url:  url,
 	}
 }
 
-type hashPrefixes []hashPrefix
+// safebrowsingList is a sorted slice of listEntries.
+type safebrowsingList []listEntry
 
-func (p hashPrefixes) sha256() []byte {
+// sha256 returns the hash of the overall list contents
+func (list safebrowsingList) sha256() []byte {
 	hash := sha256.New()
-	for _, hp := range p {
-		hash.Write([]byte(hp.hash))
+	for _, entry := range list {
+		hash.Write([]byte(entry.hash))
 	}
 	return hash.Sum(nil)
 }
 
-func (p hashPrefixes) bytes() []byte {
-	var hashes []byte
-	for _, h := range p {
-		hashes = append(hashes, []byte(h.hash)...)
+// bytes returns the overall safebrowsing list's entries all coverted to bytes
+// and concatinated together
+func (list safebrowsingList) bytes() []byte {
+	var buf []byte
+	for _, entry := range list {
+		buf = append(buf, []byte(entry.hash)...)
 	}
-	return hashes
+	return buf
 }
 
-func (p hashPrefixes) Len() int           { return len(p) }
-func (p hashPrefixes) Less(i, j int) bool { return p[i].hash < p[j].hash }
-func (p hashPrefixes) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+// safebrowsingList implements the interface required for Sorting & Search
+func (list safebrowsingList) Len() int           { return len(list) }
+func (list safebrowsingList) Less(i, j int) bool { return list[i].hash < list[j].hash }
+func (list safebrowsingList) Swap(i, j int)      { list[i], list[j] = list[j], list[i] }
 
-func (p hashPrefixes) sort() { sort.Sort(p) }
+// sort() will sort a safebrowsingList in place with sort.Sort
+func (list safebrowsingList) sort() { sort.Sort(list) }
 
-func (p hashPrefixes) searchWith(f func(int) bool) *hashPrefix {
-	return nil
-}
-
-func (p hashPrefixes) findByHash(h string) *hashPrefix {
-	i := sort.Search(len(p), func(i int) bool {
-		return p[i].hash >= h
+// findByHash searches an already sorted safebrowsingList and returns the entry
+// with hash equal to the provided hash, or nil if none is found
+func (list safebrowsingList) findByHash(h string) *listEntry {
+	i := sort.Search(len(list), func(i int) bool {
+		return list[i].hash >= h
 	})
-
-	if i < len(p) && p[i].hash == h {
-		return &p[i]
+	if i < len(list) && list[i].hash == h {
+		return &list[i]
 	}
-
 	return nil
 }
 
+// dbUpdateResponse creates a Google Safe Browsing threat list update response
+// that includes each of the test server's list entries
 func (t *testSrv) dbUpdateResponse() *gsb_proto.FetchThreatListUpdatesResponse {
+	// First we construct an overall update response to populate
 	updateResp := &gsb_proto.FetchThreatListUpdatesResponse{}
 
+	// Next, we create the full update type list update response, with an
+	// initially blank list of threat entries to process as list additions
 	addResponse := &gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse{
+		// We use the MALWARE type, ignoring platform and sending URLs
 		ThreatType:      gsb_proto.ThreatType_MALWARE,
 		PlatformType:    gsb_proto.PlatformType_ANY_PLATFORM,
 		ThreatEntryType: gsb_proto.ThreatEntryType_URL,
-		ResponseType:    gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse_FULL_UPDATE,
+		// We want this to be a "FULL UPDATE" to populate the intial DB contents
+		ResponseType: gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse_FULL_UPDATE,
 		Additions: []*gsb_proto.ThreatEntrySet{
 			&gsb_proto.ThreatEntrySet{},
 		},
 	}
 
+	// Next, we create the threat entry additions, initially leaving the raw hashes empty
 	additions := []*gsb_proto.ThreatEntrySet{
 		&gsb_proto.ThreatEntrySet{
+			// Our responses aren't compressed
 			CompressionType: gsb_proto.CompressionType_RAW,
 			RawHashes: &gsb_proto.RawHashes{
+				// We send full SHA256 hashes as "prefixes"
 				PrefixSize: sha256.Size,
 			},
 		},
 	}
 
-	// Lock the mutex for reading
+	// Lock the mutex for reading the threat list
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 
-	hashes := t.hp.bytes()
+	// Convert the list to bytes
+	hashes := t.list.bytes()
+	// Populate the raw hashes with the list bytes
 	additions[0].RawHashes.RawHashes = hashes
+	// Update the add response to have the populated additions
 	addResponse.Additions = additions
-	addResponse.Checksum = &gsb_proto.Checksum{Sha256: t.hp.sha256()}
+	// Update the add responses' checksum to be that of the overall list
+	addResponse.Checksum = &gsb_proto.Checksum{Sha256: t.list.sha256()}
 
-	// The `sblookup` client is hardcoded to expect exactly three list update
-	// responses, one for each of the threat types it cares about.
-	// We send two empty updates and one to add full hashes for the hardcoded
-	// sites. Its important to send these in the order empty, empty, non-empty
-	// because each update squashes the previous' contents (Unclear why)
+	/*
+	 * The `sblookup` client is hardcoded to expect exactly three list update
+	 * responses, one for each of the threat types it cares about. Boulder isn't
+	 * as picky, but its nice to be able to test this server using the `sblookup`
+	 * utility.
+	 *
+	 * To make this client happy we send two empty updates before the non-empty one
+	 * Its important to send these in the order empty, empty, non-empty because
+	 * each sblookup update response squashes the previous' contents (Unclear why)
+	 */
 	updateResp.ListUpdateResponses = []*gsb_proto.FetchThreatListUpdatesResponse_ListUpdateResponse{
 		emptyThreatListUpdateResp,
 		emptyThreatListUpdateResp,
 		addResponse,
 	}
-
 	return updateResp
 }
 
+// threatListUpdateFetch handles a GSB client asking for a threat list update
 func (t *testSrv) threatListUpdateFetch(w http.ResponseWriter, r *http.Request) {
+	// Unmarshal the request from the client - we ignore the contents for now and
+	// unmarshal just to check the syntax of the request
 	updateReq := &gsb_proto.FetchThreatListUpdatesRequest{}
-	err := unmarshal(r, updateReq)
+	err := unmarshalPB(r, updateReq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// Compute an update response based on the testSrv's list
 	updateResp := t.dbUpdateResponse()
-
-	err = marshal(w, updateResp)
+	// Marshal it as a response
+	err = marshalPB(w, updateResp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
 	log.Printf("Processed threatListUpdateFetch for client\n")
 }
 
+// fullHashesFind handles a GSB client asking for a specific hash to be looked
+// up in the list because it matched a local prefix.
 func (t *testSrv) fullHashesFind(w http.ResponseWriter, r *http.Request) {
+	// Unmarshal the request from the client - we use this to determine which hash
+	// they were looking for
 	findReq := &gsb_proto.FindFullHashesRequest{}
-	err := unmarshal(r, findReq)
+	err := unmarshalPB(r, findReq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// At a minimum we need a ThreatInfo with at least one ThreatEntries
 	if findReq.ThreatInfo == nil || findReq.ThreatInfo.ThreatEntries == nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
 	te := findReq.ThreatInfo.ThreatEntries
 	if len(te) < 1 {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -207,14 +248,7 @@ func (t *testSrv) fullHashesFind(w http.ResponseWriter, r *http.Request) {
 	}
 	threat := te[0]
 
-	// Lock mutex for reading
-	t.mu.RLock()
-	var match *hashPrefix
-	match = t.hp.findByHash(string(threat.Hash))
-
-	// Restore read lock immediately
-	t.mu.RUnlock()
-
+	// We start by populating an empty hash lookup response
 	resp := &gsb_proto.FindFullHashesResponse{
 		MinimumWaitDuration: &gsb_proto.Duration{
 			Seconds: 1,
@@ -224,7 +258,16 @@ func (t *testSrv) fullHashesFind(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
+	// Next we Lock mutex for reading the threat list and try to find the hash
+	// being inquired about.
+	t.mu.RLock()
+	var match *listEntry
+	match = t.list.findByHash(string(threat.Hash))
+	// Restore read lock immediately
+	t.mu.RUnlock()
+
 	if match != nil {
+		// If there was a match we need to update the response to have a ThreatMatch
 		resp.Matches = []*gsb_proto.ThreatMatch{
 			&gsb_proto.ThreatMatch{
 				ThreatType:      gsb_proto.ThreatType_MALWARE,
@@ -239,18 +282,20 @@ func (t *testSrv) fullHashesFind(w http.ResponseWriter, r *http.Request) {
 				},
 			},
 		}
-		// Lock the mutex for writing
+		// We also have to lock the mutex for writing and update the server `hits`
+		// to track that someone asked about an entry on the list
 		t.mu.Lock()
 		defer t.mu.Unlock()
 		if _, found := t.hits[match.url]; !found {
 			t.hits[match.url] = 1
 		} else {
-			t.hits[match.url] = t.hits[match.url] + 1
+			t.hits[match.url] += 1
 		}
 		log.Printf("Lookup hit for %q. Count: %d\n", match.url, t.hits[match.url])
 	}
 
-	err = marshal(w, resp)
+	// Finally we return the response to the client
+	err = marshalPB(w, resp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -258,6 +303,8 @@ func (t *testSrv) fullHashesFind(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Processed fullHashesFind for client\n")
 }
 
+// addSite processes a request to add a new URL to the mock safebrowsing list
+// Requests should include a JSON Body of the form `{"url":"example.com"}`
 func (t *testSrv) addSite(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Request method must be POST", http.StatusMethodNotAllowed)
@@ -278,12 +325,14 @@ func (t *testSrv) addSite(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// The safe browsing library looks up URLs with a trailing slash and expects
+	// the DB contents/hashes to reflect that. We add the slash here as required
 	if !strings.HasSuffix(urlData.Url, "/") {
 		urlData.Url += "/"
 	}
 
-	newHp := newHashPrefix(urlData.Url)
-	match := t.hp.findByHash(newHp.hash)
+	newHp := newListEntry(urlData.Url)
+	match := t.list.findByHash(newHp.hash)
 	if match != nil {
 		log.Printf("Skipped adding %q - Already on the list\n", newHp.url)
 		return
@@ -294,11 +343,19 @@ func (t *testSrv) addSite(w http.ResponseWriter, r *http.Request) {
 	defer t.mu.Unlock()
 
 	// Add the new hash prefix and resort the list
-	t.hp = append(t.hp, newHp)
-	t.hp.sort()
+	t.list = append(t.list, newHp)
+	t.list.sort()
 	log.Printf("Added %q to safe browsing list\n", urlData.Url)
 }
 
+// getHits returns a JSON object describing how many times each URL on the mock
+// safebrowsing list was asked about by a client. E.g.
+// ```
+// {
+//   "evil.com": 2,
+//   "example.com": 0
+// }
+// ```
 func (t *testSrv) getHits(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -322,7 +379,10 @@ func (t *testSrv) getHits(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Processed /hits request for client\n")
 }
 
-func (t *testSrv) gsbHandler(inner http.HandlerFunc) http.HandlerFunc { //w *http.ResponseWriter, r *http.Request) {
+// gsbHandler creates an http.HandlerFunc that wraps the `inner` handler. The
+// wrapper will check that the request is a POST, has the correct content-type,
+// and that the `key` GET parameter matches the server's API key.
+func (t *testSrv) gsbHandler(inner http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// The HTTP method must be POST
 		if r.Method != "POST" {
@@ -345,6 +405,7 @@ func (t *testSrv) gsbHandler(inner http.HandlerFunc) http.HandlerFunc { //w *htt
 	})
 }
 
+// start() sets up the HTTP muxer and spawns a Go routine to ListenAndServe
 func (t *testSrv) start(listenAddr string) {
 	mux := http.NewServeMux()
 	mux.Handle("/v4/threatListUpdates:fetch", t.gsbHandler(t.threatListUpdateFetch))
@@ -361,6 +422,8 @@ func (t *testSrv) start(listenAddr string) {
 	}()
 }
 
+// newTestServer constructs a testSrv instance with its list constructed from
+// the provided slice of strings.
 func newTestServer(apiKey string, unsafeURLs []string) testSrv {
 	ts := testSrv{
 		apiKey: apiKey,
@@ -368,24 +431,24 @@ func newTestServer(apiKey string, unsafeURLs []string) testSrv {
 		mu:     new(sync.RWMutex),
 	}
 
-	var hp hashPrefixes
+	var initialList safebrowsingList
 	for _, s := range unsafeURLs {
+		// The safe browsing library looks up URLs with a trailing slash and expects
+		// the DB contents/hashes to reflect that. We add the slash here as required
 		if !strings.HasSuffix(s, "/") {
 			s += "/"
 		}
-
-		hp = append(hp, newHashPrefix(s))
+		initialList = append(initialList, newListEntry(s))
 	}
-	hp.sort()
-	ts.hp = hp
-
+	initialList.sort()
+	ts.list = initialList
 	return ts
 }
 
+// main() processes command line flags, creates, and starts a `testSrv`
 func main() {
 	key := flag.String("apikey", "", "API key for client access")
 	listen := flag.String("listenAddress", ":6000", "Listen address for HTTP server")
-
 	flag.Parse()
 
 	log.SetPrefix("gsb-test-srv: ")
@@ -396,7 +459,7 @@ func main() {
 	}
 
 	log.Printf("Starting GSB Test Server on %q\n", *listen)
-	ts := newTestServer(*key, []string{"evil.com", "malware.biz"})
+	ts := newTestServer(*key, defaultUnsafeURLs)
 	ts.start(*listen)
 
 	// Block on an empty channel
