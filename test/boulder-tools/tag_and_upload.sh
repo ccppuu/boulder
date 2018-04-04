44699ac285bd9eaddbea1b674b7a1d8bc18c211f
Daniel <cpu@letsencrypt.org>
2018-04-04 16:30:41 -0400

    WIP

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -3,15 +3,40 @@
 cd $(dirname $0)
 
 DATESTAMP=$(date +%Y-%m-%d)
-TAG_NAME="letsencrypt/boulder-tools:$DATESTAMP"
+BASE_TAG_NAME="letsencrypt/boulder-tools"
+GO_VERSIONS=( "1.10" "1.10.1" )
 
-echo "Building boulder-tools image $TAG_NAME"
-docker build . -t $TAG_NAME --no-cache
+# Build a tagged image for each GO_VERSION
+for GO_VERSION in "${GO_VERSIONS[@]}"
+do
+  TAG_NAME="$BASE_TAG_NAME-go$GO_VERSION:$DATESTAMP"
+  echo "Building boulder-tools image $TAG_NAME"
 
-echo "Image ready."
+  # TODO(@cpu): proper tempfile?
+  DOCKERFILE="golang.$GO_VERSION.Dockerfile"
+
+  # TODO(@cpu): Comment the unfortunate WHY of this
+  sed -r \
+    -e 's!%%GO_VERSION%%!'"$GO_VERSION"'!g' \
+    "Dockerfile.tmpl" > "$DOCKERFILE"
+
+  docker build . \
+    -t $TAG_NAME \
+    --no-cache \
+    -f "$DOCKERFILE"
+
+  # TODO(@cpu): Move this to an at-exit handler
+  rm "$DOCKERFILE"
+done
+
+# Log in once now that images are ready to upload
+echo "Images ready, please login to allow Dockerhub push"
 docker login
 
-echo "Pushing $TAG_NAME to Dockerhub"
-docker push $TAG_NAME
-
-sed -i "s,image: letsencrypt/boulder-tools.*,image: $TAG_NAME," ../../docker-compose.yml
+# Upload a tagged image for each GO_VERSION
+for GO_VERSION in "${GO_VERSIONS[@]}"
+do
+  TAG_NAME="$BASE_TAG_NAME-go$GO_VERSION:$DATESTAMP"
+  echo "Pushing $TAG_NAME to Dockerhub"
+  docker push $TAG_NAME
+done
